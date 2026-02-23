0894cc6c323f8800192b1c3e226ffafca690f5d1
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-23 13:34:36 +0100

    cambio

--- code/src/main/java/com/endes/figuras/punto.java
+++ code/src/main/java/com/endes/figuras/punto.java
@@ -7,4 +7,16 @@ public class punto extends FiguraGeometrica {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
