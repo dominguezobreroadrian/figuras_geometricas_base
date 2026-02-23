475d4a49fa6fecf5f551d4c1cc47ffdf10b4afb7
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-23 13:15:03 +0100

    punto

---
+++ code/src/main/java/com/endes/figuras/punto.java
@@ -0,0 +1,10 @@
+package com.endes.figuras;
+
+public class punto extends FiguraGeometrica {
+
+	public punto(String nombre, String color) {
+		super(nombre, color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
