30c22c3db61e0835f8e3cccbb8dfa929d0244bd7
carlosruizgarrido <ruiz.garrido.carlos.manuel@iescamas.es>
2026-02-23 13:41:49 +0100

    Clase Circulo terminada(?)

--- code/src/main/java/com/endes/figuras/Circulo.java
+++ code/src/main/java/com/endes/figuras/Circulo.java
@@ -1,3 +1,26 @@
-public class Circulo {
-    
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double perimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    public void rotar(double angulo) {
+        super.rotar(angulo);
+    }
+
+    public void trasladar(double dx, double dy) {
+        super.trasladar(dx, dy);
+    }
 }
