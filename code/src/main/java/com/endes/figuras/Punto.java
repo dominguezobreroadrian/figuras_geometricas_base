76055bcb4e641dfab36abc86c33789c163d19c80
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-02-23 14:09:06 +0100

    Refactor Punto class with updated distance calculation

--- code/src/main/java/com/endes/figuras/Punto.java
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -3,30 +3,46 @@ package com.endes.figuras;
 public class Punto {
 	private double x;
 	private double y;
+
+	/**
+     * Constructor para crear un punto con coordenadas específicas.
+     * @param x Posición en el eje X.
+     * @param y Posición en el eje Y.
+     */
 	
 	public Punto(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-
+	// --- Getters ---
 	public double getX() {
 		return x;
 	}
-	
-	public void setX() {
-		this.x = x;
-	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
-	public void setY() {
-		this.y = y;
+	// --- Setters ---
+	public void setX(double x) {
+		this.x = x;
 	}
 	
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	/**
+     * Calcula la distancia entre este punto y otro punto dado.
+     * Utiliza la fórmula: raíz((x2-x1)² + (y2-y1)²)
+     * 
+     * @param otro El otro punto hacia el cual calcular la distancia.
+     * @return La distancia euclidiana como un valor double.
+     */
+	
 	public double distancia(Punto otro) {
-		return Math.sqrt(((otro.getX() - this.x)*2) + ((otro.getY() - this.y)*2));
+		double dx = otro.getX() - this.x;
+        double dy = otro.getY() - this.y;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 	}
 	
 	}
