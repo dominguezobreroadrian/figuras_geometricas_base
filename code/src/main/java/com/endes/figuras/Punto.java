1ccf79c60065141d4e6d118e1a1f5c49a1fa9d27
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-23 14:05:03 +0100

    punto terminado

---
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -0,0 +1,32 @@
+package com.endes.figuras;
+
+public class Punto {
+	private double x;
+	private double y;
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+	
+	public void setX() {
+		this.x = x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setY() {
+		this.y = y;
+	}
+	
+	public double distancia(Punto otro) {
+		return Math.sqrt(((otro.getX() - this.x)*2) + ((otro.getY() - this.y)*2));
+	}
+	
+	}
